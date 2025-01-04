db7541b2cdffce1744ec92feddb03a7c4ee9bc7c
Davide <82649871+davidecarluccio@users.noreply.github.com>
2025-01-04 19:41:54 +0100

    Provided my solution

--- README.md
+++ README.md
@@ -1,3 +1,19 @@
-# Request Header Parser Microservice
+Request Header Parser Microservice
+==================================
 
-This is the boilerplate for the Request Header Parser Microservice project. Instructions for building your project can be found at https://www.freecodecamp.org/learn/apis-and-microservices/apis-and-microservices-projects/request-header-parser-microservice
+Build a full stack JavaScript app that is functionally similar to this: [https://request-header-parser-microservice.freecodecamp.rocks/](https://request-header-parser-microservice.freecodecamp.rocks/). Working on this project will involve you writing your code using one of the following methods:
+
+*   Clone [this GitHub repo](https://github.com/freeCodeCamp/boilerplate-project-headerparser/) and complete your project locally.
+*   Use [our Gitpod starter project](https://gitpod.io/?autostart=true#https://github.com/freeCodeCamp/boilerplate-project-headerparser/) to complete your project. Learn [how to share your Gitpod workspace to get help](https://forum.freecodecamp.org/t/how-to-use-gitpod-in-the-curriculum/668669#how-can-i-share-my-workspace-to-get-help-8).
+*   Use a site builder of your choice to complete the project. Be sure to incorporate all the files from our GitHub repo.
+
+Tests
+-----
+
+1\. You should provide your own project, not the example URL.
+    
+2\. A request to `/api/whoami` should return a JSON object with your IP address in the `ipaddress` key.
+    
+3\. A request to `/api/whoami` should return a JSON object with your preferred language in the `language` key.
+    
+4\. A request to `/api/whoami` should return a JSON object with your software in the `software` key.
--- index.js
+++ index.js
@@ -1,30 +1,44 @@
 // index.js
 // where your node app starts
 
-// init project
+// Initialize project
 require('dotenv').config();
 var express = require('express');
 var app = express();
 
-// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC
+// Enable CORS for remote testing
 var cors = require('cors');
 app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
 
-// http://expressjs.com/en/starter/static-files.html
+// Serve static files (if needed)
 app.use(express.static('public'));
 
-// http://expressjs.com/en/starter/basic-routing.html
+// Route for the main page (you can replace this if you want a more detailed frontend)
 app.get('/', function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
-// your first API endpoint...
+// Hello API endpoint
 app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
 });
 
-// listen for requests :)
+// The Request Header Parser endpoint
+app.get('/api/whoami', function (req, res) {
+  // Extract the IP address, language, and software from the headers
+  const ipAddress = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+  const language = req.headers['accept-language']?.split(',')[0];
+  const software = req.headers['user-agent'].match(/\(([^)]+)\)/)[1];
+
+  // Return the parsed information in JSON format
+  res.json({
+    ipaddress: ipAddress,
+    language: language,
+    software: software
+  });
+});
+
+// Start the server
 var listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
